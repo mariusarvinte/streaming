b5eebb83cd29db4efd225afc54323749dca0b300
Marius Arvinte <mariusarvinte@gmail.com>
2025-12-27 19:30:06 -0800

    Code for 12/27/2025 stream

---
+++ src/stream/adapter.py
@@ -0,0 +1,177 @@
+import os
+
+from pathlib import Path
+from typing import Any
+
+from dataclasses import dataclass, field
+from functools import cached_property
+
+import dspy
+from dspy import Signature
+from dspy.clients.lm import LM
+from dspy.adapters.types.base_type import split_message_content_for_custom_types
+
+
+@dataclass
+class File:
+    path: Path
+    depends_on: list[Path] = field(default_factory=list)
+
+
+@dataclass
+class Folder:
+    path: Path
+    files: list[File] = field(default_factory=list)
+    subfolders: list[Folder] = field(default_factory=list)
+
+    def initialize_modules(self) -> None:
+        os.makedirs(self.path, exist_ok=True)
+        (self.path / "__init__.py").touch()
+
+        for folder in self.subfolders:
+            folder.initialize_modules()
+
+    @cached_property
+    def dependency_map(self) -> dict[str, list[Path]]:
+        mapping = {f.path.stem: f.depends_on for f in self.files}
+        for folder in self.subfolders:
+            mapping.update(folder.dependency_map)
+
+        return mapping
+
+    @cached_property
+    def file_map(self) -> dict[str, Path]:
+        mapping = {f.path.stem: f.path for f in self.files}
+        for folder in self.subfolders:
+            mapping.update(folder.file_map)
+
+        return mapping
+
+
+# Custom adapter that injects instructions about cross-output dependencies
+class FileAdapter(dspy.ChatAdapter):
+    def __call__(
+        self,
+        lm: "LM",
+        lm_kwargs: dict[str, Any],
+        signature: type[Signature],
+        demos: list[dict[str, Any]],
+        inputs: dict[str, Any],
+    ) -> list[dict[str, Any]]:
+        self.project = signature.input_fields["project"]
+        signature = signature.delete("project")
+
+        # TODO: Re-order the output fields in topological order
+        processed_signature = self._call_preprocess(lm, lm_kwargs, signature, inputs)
+        inputs = self.format(processed_signature, demos, inputs)
+
+        outputs = lm(messages=inputs, **lm_kwargs)
+        return self._call_postprocess(
+            processed_signature, signature, outputs, lm, lm_kwargs
+        )
+
+    def format_output_interactions(
+        self,
+        signature: type[Signature],
+    ) -> str:
+        output = f""
+
+        # Helper function that generates a language-dependent use statement
+        def generate_use_statement(
+            location: str,
+            language: str,
+        ):
+            if language.lower() == "python":
+                # NOTE: We could statically get the function name here, if the outputs are processed in topological order
+                parts = list(location.parts)
+                module = location.stem
+                parts[-1] = module
+                return f"from {'.'.join(parts)} import <function-name>"
+            else:
+                raise ValueError(f"Use statements not yet implement for {language = }!")
+
+        # Cache use statements for each pair of locations
+        use_statements = dict()
+        info: Folder = self.project.json_schema_extra["desc"]
+
+        for field in signature.output_fields.keys():
+            depends_on: list[Path] = [f.stem for f in info.dependency_map[field]]
+            # Early exit
+            if not depends_on:
+                continue
+
+            # Introductory instructions
+            output += f"When generating code for `{field}`, use the following import statements from other outputs:\n"
+
+            # Generate use statements and instructions for each dependency
+            all_fields = signature.input_fields | signature.output_fields
+            for dep in depends_on:
+                location: Path = info.file_map[dep]
+                if getattr(all_fields[dep].annotation, "__bases__", None) == (
+                    dspy.Code,
+                ) and (
+                    all_fields[dep].annotation.language
+                    != all_fields[field].annotation.language
+                ):
+                    raise ValueError("Cannot generate cross-language use statements!")
+
+                if (field, dep) in use_statements:
+                    use_statement = use_statements[(field, dep)]
+                else:
+                    use_statement: str = generate_use_statement(
+                        location,
+                        all_fields[field].annotation.language,
+                    )
+                    # Cache it
+                    use_statements[(field, dep)] = use_statement
+
+                # Add to instructions
+                output += f"{use_statement}\n"
+
+        return output
+
+    def format(
+        self,
+        signature: type[Signature],
+        demos: list[dict[str, Any]],
+        inputs: dict[str, Any],
+    ) -> list[dict[str, Any]]:
+        inputs_copy = dict(inputs)
+
+        # If the signature and inputs have conversation history, we need to format the conversation history and
+        # remove the history field from the signature.
+        history_field_name = self._get_history_field_name(signature)
+        if history_field_name:
+            # In order to format the conversation history, we need to remove the history field from the signature.
+            signature_without_history = signature.delete(history_field_name)
+            conversation_history = self.format_conversation_history(
+                signature_without_history,
+                history_field_name,
+                inputs_copy,
+            )
+
+        messages = []
+        system_message = (
+            f"{self.format_field_description(signature)}\n"
+            f"{self.format_field_structure(signature)}\n"
+            f"{self.format_output_interactions(signature)}\n"
+            f"{self.format_task_description(signature)}"
+        )
+        messages.append({"role": "system", "content": system_message})
+        messages.extend(self.format_demos(signature, demos))
+        if history_field_name:
+            # Conversation history and current input
+            content = self.format_user_message_content(
+                signature_without_history, inputs_copy, main_request=True
+            )
+            messages.extend(conversation_history)
+            messages.append({"role": "user", "content": content})
+        else:
+            # Only current input
+            content = self.format_user_message_content(
+                signature, inputs_copy, main_request=True
+            )
+            messages.append({"role": "user", "content": content})
+
+        messages = split_message_content_for_custom_types(messages)
+        return messages
---
+++ src/stream/main.py
@@ -0,0 +1,138 @@
+import argparse
+import os
+import sys
+
+from pathlib import Path
+import dspy
+
+from stream.adapter import FileAdapter
+from stream.adapter import File, Folder
+
+
+def main(args):
+    lm = dspy.LM(
+        model="openrouter/nvidia/nemotron-3-nano-30b-a3b:free",
+        api_key=os.environ["OPENROUTER_API_KEY"],
+        temperature=1.0,
+        cache=False,
+    )
+    adapter = FileAdapter() if args.files else dspy.ChatAdapter()
+    dspy.configure(
+        lm=lm,
+        adapter=adapter,
+    )
+
+    # Define the project (e.g., for Python, package) structure
+    project_name = "./output"
+    structure: Folder = Folder(
+        path=Path(f"{project_name}"),
+        files=[
+            File(Path(f"{project_name}/cases.py")),
+            File(Path(f"{project_name}/solution.py")),
+            File(
+                Path(f"{project_name}/test.py"),
+                depends_on=[
+                    Path(f"{project_name}/cases.py"),
+                    Path(f"{project_name}/solution.py"),
+                    Path(f"{project_name}/measurements/runtime.py"),
+                    Path(f"{project_name}/measurements/memory.py"),
+                ],
+            ),
+        ],
+        subfolders=[
+            Folder(
+                path=Path(f"{project_name}/measurements"),
+                files=[
+                    File(
+                        Path(f"{project_name}/measurements/runtime.py"),
+                        depends_on=[
+                            Path(f"{project_name}/cases.py"),
+                            Path(f"{project_name}/solution.py"),
+                        ],
+                    ),
+                    File(
+                        Path(f"{project_name}/measurements/memory.py"),
+                        depends_on=[
+                            Path(f"{project_name}/cases.py"),
+                            Path(f"{project_name}/solution.py"),
+                        ],
+                    ),
+                ],
+            )
+        ],
+    )
+
+    class ProblemSolving(dspy.Signature):
+        """You are an expert in solving algorithmic problems using Python."""
+
+        project: Folder = dspy.InputField(desc=structure)
+        problem: str = dspy.InputField(
+            desc="The description of the problem to be solved",
+        )
+        cases: list[tuple[list[int], list[int]]] = dspy.InputField(
+            desc="A list of paired inputs and outputs for the problem",
+        )
+
+        solution: dspy.Code["Python"] = dspy.OutputField(
+            desc="Code that when executed with the inputs, produces the expected outputs",
+        )
+        runtime: dspy.Code["Python"] = dspy.OutputField(
+            desc="Code that profiles the execution time of `solution` when running on `cases`",
+        )
+        memory: dspy.Code["Python"] = dspy.OutputField(
+            desc="Code that profiles the runtime memory usage of `solution` when running on `cases`",
+        )
+        test: dspy.Code["Python"] = dspy.OutputField(
+            desc="Code that tests the `solution` on the provided `cases` and runs `runtime` and `memory` measurements",
+        )
+
+    # Define an AI module that is templated (prompted) to solve the task
+    module = dspy.Predict(ProblemSolving)
+
+    # Perform the task on some inputs
+    inputs = {
+        "project": None,
+        "problem": """Given an integer array `nums` sorted in non-decreasing order, consider the number of unique elements in `nums` to be `k`.
+    Return the number of unique elements `k`.""",
+        "cases": [
+            ([1, 1, 4, 6, 7, 9], [5]),
+            ([4, 4, 4, 4, 4], [1]),
+            ([26, 999, 1003], [3]),
+        ],
+    }
+    pred = module(**inputs)
+
+    # Save the original stdout to restore it later
+    original_stdout = sys.stdout
+    with open("./history.txt", "w") as f:
+        # Redirect stdout to the file
+        sys.stdout = f
+        dspy.inspect_history(n=1)
+    # Restore stdout to the original (usually the console)
+    sys.stdout = original_stdout
+
+    # Write the Python code to files
+    structure.initialize_modules()
+    for key in ProblemSolving.output_fields.keys():
+        with open(structure.file_map[key], "w") as f:
+            f.write(pred[key].code)
+
+    # FIXME: Figure out how to annotate data structures to-be-converted to code
+    written_inputs = ["cases"]
+    for key in written_inputs:
+        with open(structure.file_map[key], "w") as f:
+            f.write(f'{key} = {inputs[key]}')
+
+
+if __name__ == "__main__":
+    sys.stdout.reconfigure(encoding="utf-8")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--files",
+        action="store_true",
+        help="Use file-system aware adapter and signature",
+    )
+    args = parser.parse_args()
+
+    main(args)
